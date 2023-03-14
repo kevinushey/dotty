5587bae9cd742829620bc89e4a328e4f7d75911a
Kevin Ushey <kevinushey@gmail.com>
2023-03-14 14:51:31 -0700

    hack codetools to allow destructuring assign

--- R/dotify.R
+++ R/dotify.R
@@ -1,38 +1,47 @@
 
 dotify <- function() {
 
-  files <- list.files("R", full.names = TRUE)
-  for (file in files)
-    dotify_file(file)
-}
-
-dotify_file <- function(file) {
-  expr <- parse(file, keep.source = FALSE)
-  dotify_expr(expr)
-}
-
-dotify_expr <- function(expr) {
-
-  if (identical(expr, quote(expr = )))
+  # use some .onLoad() hacks to tell codetools how to parse our code
+  if (!requireNamespace("codetools", quietly = TRUE))
     return()
 
-  dotty <-
-    is.call(expr) &&
-    identical(expr[[1L]], as.name("<-")) &&
-    is.call(expr[[2L]]) &&
-    identical(expr[[2L]][[1L]], as.name("[")) &&
-    identical(expr[[2L]][[2L]], as.name("."))
+  # make sure we have a handlers environment
+  codetools <- asNamespace("codetools")
+  handlers <- codetools$collectUsageHandlers
+  if (!is.environment(handlers))
+    return()
 
-  if (dotty) {
-    dotcall <- expr[[2L]]
-    for (i in 3L:length(dotcall)) {
-      name <- as.character(dotcall[[i]])
-      globalVariables(name, package = .packageName)
+  # tell codetools to accept our code's handlers
+  # TODO: ask Luke nicely to allow us to do this
+  hack <- function(v, env) TRUE
+  environment(hack) <- codetools
+  .BaseNamespaceEnv$unlockBinding("isUtilsVar", env = codetools)
+  assign("isUtilsVar", hack, envir = codetools)
+  .BaseNamespaceEnv$lockBinding("isUtilsVar", env = codetools)
+
+  # add our handler for subset-assignment
+  handler <- handlers$`[<-`
+  handlers$`[<-` <- function(v, w) {
+
+    # get defined variables
+    vars <- character()
+    for (i in seq_along(v)) {
+      if (is.symbol(v[[i]])) {
+        vars <- c(vars, as.character(v[[i]]))
+      }
     }
+
+    # collect locals with these treated as parameters
+    w$startCollectLocals(vars, character(), w)
+
+    # call original handler
+    if (!is.null(handler))
+      handler(v, w)
+
   }
 
-  if (is.recursive(expr))
-    for (i in seq_along(expr))
-      dotify_expr(expr[[i]])
+  # included just for testing
+  .[a, b] <- c(1, 2)
+  c(a, b)
 
 }
